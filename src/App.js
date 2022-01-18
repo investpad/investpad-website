83f9f2c9a3e59a22dc5f73d404588635cbe658d2
heycryptobob <heycryptobob@gmail.com>
2022-01-18 15:27:05 +0200

    added overview section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Overview from "./sections/Overview";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Overview />
     </>
   );
 }
